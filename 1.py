8663299f3d813d21e882670301e153d99f56321c
alok07567 <98089950+alok07567@users.noreply.github.com>
2022-01-20 16:36:57 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,18 @@
+# print('Alok is a "GOOD" boy')
+# print("Alok is a 'GOOD' boy")
+# print('Alok is a """very very "GOOD" boy"""')
+# #Alok is a good boy
+# print(""" 
+#     Alok\
+#     is\
+#     very\
+#     very\
+#     very..\
+#     good\
+#     BOY\
+# """)
+# if 5>2:
+#     print("Hello",end=" ")
+#     print('Chal hatttt',end=" ")
+#     print("""What the hell is going on""",end=" ")
+print("Alok\nRanjan\nJoshi")
---
+++ 2.py
@@ -0,0 +1,7 @@
+i=63
+print("Value of i is: ",i)
+print("Char value of i is: %c"%i)
+print("Float value of i is: %f"%i)
+# print("Binary value of i is: %b"%i)
+print("Octal value of i is: %o"%i)
+print("Headecimal value of i is: %x"%i)
